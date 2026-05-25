27b893b1cf270f0f25c8c1bc00b0d056cbab8d59
binhtt <binhtt75@gmail.com>
2026-05-25 08:23:10 +0700

    Add describe_refactoring function with refactoring rules

---
+++ src/refactoring.py
@@ -0,0 +1,9 @@
+def describe_refactoring():
+
+    return [
+        "Rule extraction",
+        "Rule merging",
+        "Dead rule elimination",
+        "Condition simplification",
+        "Priority restructuring"
+    ]
