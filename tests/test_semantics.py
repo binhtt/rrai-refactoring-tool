05304ea3be2cd6720a1feada75e7a2cb6e14768b
binhtt <binhtt75@gmail.com>
2026-05-25 09:02:00 +0700

    Rename test_move to test_move_forward and add tests

--- tests/test_semantics.py
+++ tests/test_semantics.py
@@ -1,10 +1,41 @@
 from semantics import delta
 
 
-def test_move():
+def test_move_forward():
 
     state = {}
 
-    result = delta(state, "moveForward")
+    result = delta(
+        state,
+        "moveForward"
+    )
 
     assert result["moving"] is True
+
+
+def test_emergency_stop():
+
+    state = {
+        "moving": True
+    }
+
+    result = delta(
+        state,
+        "emergencyStop"
+    )
+
+    assert result["moving"] is False
+
+    assert result["emergency"] is True
+
+
+def test_turn_left():
+
+    state = {}
+
+    result = delta(
+        state,
+        "turnLeft"
+    )
+
+    assert result["direction"] == "left"
